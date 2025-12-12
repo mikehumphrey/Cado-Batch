964f8f72036c2dc982566d7c426fadb0294144c1
Mike Humphrey <mike.humphrey@me.com>
2025-12-12 08:50:10 -0900

    Update README and collect.bat for PowerShell script modernization and improved data collection

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Cado-Batch
-A Batch script to collect forensic evidence from older Windows systems such as Windows Server 2003
+# Cado Evidence Collector
+
+A PowerShell script to collect forensic evidence from modern Windows systems (Windows 10, Windows 11, Server 2016+). This script modernizes the functionality of the original `collect.bat`.
+
+## Data Collected
+
+The script collects the following forensic artifacts:
+
+-   **NTFS Metadata**:
+    -   `$MFT` (Master File Table) from the system drive.
+    -   `$LogFile` (NTFS Log File) from the system drive.
+-   **Windows Event Logs**:
+    -   All `.evtx` files from `%SystemRoot%\System32\winevt\logs\`.
+-   **Registry Hives**:
+    -   System hives: `SYSTEM`, `SOFTWARE`, `SAM`, `SECURITY`, `DEFAULT` from `%SystemRoot%\System32\Config`.
+    -   Administrator's user hive: `NTUSER.DAT` from the default administrator profile.
+-   **System Information**:
+    -   A recursive directory listing of the root of the system drive (`C:\`).
+
+All collected data is placed in a `collected_files` directory and then compressed into a single `collected_files.zip` archive.
 
 # How to execute
-* Download https://github.com/cado-security/Cado-Batch/releases/download/Release/Cado-Batch-Release.zip and extract on the target system
-* Run collect.bat as Administrator
-* This will then create a file "collected_files.zip" which can be imported into a forensic processing platform such as Cado Response
+
+1.  Open PowerShell with administrative privileges.
+2.  Navigate to the script directory.
+3.  Run the script: `.\collect.ps1`
+
+For detailed, real-time output of the script's actions, use the `-Verbose` flag:
+
+```powershell
+.\collect.ps1 -Verbose
+```
+
+This will create a file "collected_files.zip" which can be imported into a forensic processing platform such as Cado Response.
--- collect.bat
+++ collect.bat
@@ -16,36 +16,22 @@ mkdir collected_files
 echo Collecting $MFT and $LogFile
 bins\RawCopy.exe /FileNamePath:C:0
 bins\RawCopy.exe /FileNamePath:c:\$LogFile
-move bins\$MFT collected_files\MFT.exported
+move bins\$MFT collected_files\MFT_C.bin
 move bins\$LogFile collected_files\LogFile_C.bin
 
 echo Collecting EVTX files and System Registry
 xcopy %SystemRoot%\System32\Config collected_files\ /E /Y /C /Q
 xcopy %SystemRoot%\system32\winevt\logs\*.evtx collected_files\ /E /Y /C /Q
+bins\RawCopy.exe /FileNamePath:"C:\Documents and Settings\Administrator\NTUSER.DAT"
+move bins\NTUSER.dat collected_files\Admin_NTUSER.DAT
+
 bins\RawCopy.exe /FileNamePath:C:\WINDOWS\system32\config\SYSTEM
 move bins\SYSTEM collected_files\SYSTEM
-
-echo Collecting User Hives
-if exist C:\Users\ (
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Users\"') do mkdir "collected_files\%%G"
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Users\"') do if exist "C:\Users\%%G\NTUSER.DAT" (bins\RawCopy.exe /FileNamePath:"C:\Users\%%G\NTUSER.DAT" /OutputPath:"collected_files\%%G\")
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Users\"') do if exist "C:\Users\%%G\AppData\Local\Microsoft\Windows\UsrClass.dat" (bins\RawCopy.exe /FileNamePath:"C:\Users\%%G\AppData\Local\Microsoft\Windows\UsrClass.dat" /OutputPath:"collected_files\%%G\")
-) else (
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Documents and Settings\"') do mkdir "collected_files\%%G"
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Documents and Settings\"') do if exist "C:\Documents and Settings\%%G\NTUSER.DAT" (bins\RawCopy.exe /FileNamePath:"C:\Documents and Settings\%%G\NTUSER.DAT" /OutputPath:"collected_files\%%G\")
-    for /f "tokens=*" %%G in ('dir /A:D /B "C:\Documents and Settings\"') do if exist "C:\Documents and Settings\%%G\Local Settings\Application Data\Microsoft\Windows\UsrClass.dat" bins\RawCopy.exe /FileNamePath:"C:\Documents and Settings\%%G\Local Settings\Application Data\Microsoft\Windows\UsrClass.dat" /OutputPath:"collected_files\%%G\"
-)
 
 echo Listing Root Directory
 dir c:\ > collected_files\C_Dir.txt
 
-set hour=%time:~0,2%
-set min=%time:~3,2%
-set sec=%time:~6,2%
-
-set ftime=%hour%%min%%sec%
-
 echo Compressing into collected_files.zip
-bins\zip.exe -r CadoBatch_%computername%_%ftime%.zip collected_files
+bins\zip.exe -r collected_files.zip collected_files
 
 pause
